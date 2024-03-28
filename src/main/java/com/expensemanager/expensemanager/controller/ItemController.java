214ff1a8513c7c528e42729b3c0216609b0b6972
Sakshi <satshriyakalvaani@gmail.com>
2024-03-28 23:26:24 +0530

    Added getItems method to Item controller responding with list<string> format

--- src/main/java/com/expensemanager/expensemanager/controller/ItemController.java
+++ src/main/java/com/expensemanager/expensemanager/controller/ItemController.java
@@ -2,12 +2,13 @@ package com.expensemanager.expensemanager.controller;
 
 
 import com.expensemanager.expensemanager.model.Item;
+import com.expensemanager.expensemanager.model.head;
+import com.expensemanager.expensemanager.repo.headrepo;
 import com.expensemanager.expensemanager.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/director/item")
@@ -15,6 +16,9 @@ public class ItemController {
 
     @Autowired
     ItemService its;
+
+    @Autowired
+    headrepo headr;
 
     @PostMapping("/addItem")
     public boolean addItem(@RequestParam String headname , @RequestParam String itemname){
@@ -29,4 +33,17 @@ public class ItemController {
 
 
     }
+
+
+    @GetMapping("/getItems")
+    List<String> getItems(@RequestParam String headname)throws Exception{
+        head h = headr.findByHeadName(headname);
+        try{
+            return its.getAllItems(h.getHeadId());
+        }
+        catch(Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+
 }
